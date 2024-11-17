80c1773f9034e77151424bb97ceedff35e9bf810
Tusher <tusher1612@gmail.com>
2024-11-17 09:08:46 -0600

    openai key changed and deployed

---
+++ convex/auth.config.ts
@@ -0,0 +1,11 @@
+const authConfig = {
+  providers: [
+    {
+      domain: "https://intense-gannet-38.clerk.accounts.dev",
+      
+      applicationID: "convex",
+    },
+  ]
+};
+
+export default authConfig;
